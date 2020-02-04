18465dc505e5e95e28839b91655c98a3c39f4da7
Michael Cooper <macoop2363@gmail.com>
2020-02-04 12:31:54 -0700

    Added initial test for Favorite creation

---
+++ tests/requests/api/v1/favorites/create.spec.js
@@ -0,0 +1,51 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../../../../../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
+
+
+describe('Test the favorites path', () => {
+  beforeEach(async () => {
+    await database.raw('truncate table favorites cascade');
+  });
+
+  afterEach(() => {
+    database.raw('truncate table favorites cascade');
+  });
+
+  describe('test favorites creation', () => {
+    it('happy path', async () => {
+      const res = await request(app)
+        .post("/api/v1/favorites")
+        .send({
+          title: 'We Will Rock You',
+          artist: 'Queen'
+        });
+
+      let favorites = await database('favorites').select()
+      expect(favorites.length).toBe(1);
+      
+      expect(res.statusCode).toBe(201);
+      expect(res.body).toHaveProperty('id');
+      expect(res.body).toHaveProperty('title');
+      expect(res.body).toHaveProperty('artistName');
+      expect(res.body).toHaveProperty('genre');
+      expect(res.body).toHaveProperty('rating');
+    });
+    
+    it('sad path', async () => {
+      const res = await request(app)
+        .post("/api/v1/favorites")
+        .send({
+          title: '',
+          artist: ''
+        });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toHaveProperty('message');
+    });
+  });
+});
