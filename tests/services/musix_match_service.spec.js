ef4c1862acb181bcc5ca3da61d31fe77a0a5ada8
Michael Cooper <macoop2363@gmail.com>
2020-02-04 12:05:35 -0700

    Changed the query endpoint to get back the right response

--- tests/services/musix_match_service.spec.js
+++ tests/services/musix_match_service.spec.js
@@ -8,7 +8,10 @@ describe('Test ', () => {
 
     expect(response).toHaveProperty('message')
     expect(response.message).toHaveProperty('body')
-    expect(response.message.body).toHaveProperty('track_list')
-    expect(response.message.body.track_list[0]).toHaveProperty('track')
+    expect(response.message.body).toHaveProperty('track')
+    expect(response.message.body.track).toHaveProperty('track_name')
+    expect(response.message.body.track.track_name).toBe('We Will Rock You')
+    expect(response.message.body.track).toHaveProperty('artist_name')
+    expect(response.message.body.track.artist_name).toBe('Queen')
   });
 });
