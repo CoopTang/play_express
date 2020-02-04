f1d0a24b3955eb7d0038a1a8aac223409a626690
Jon Patterson <JonPatt92@gmail.com>
2020-02-04 12:04:15 -0700

    Add non rated or genre record to test how schema reacts with default values

--- db/seeds/dev/favorites.js
+++ db/seeds/dev/favorites.js
@@ -8,6 +8,7 @@ exports.seed = function(knex) {
         {id: 1, title: 'Take On Me', artist_name: 'a-ha', rating: 87, genre: 'Pop'},
         {id: 2, title: 'Coming Home', artist_name: 'Leon Bridges', rating: 90, genre: 'Soul'},
         {id: 3, title: 'Africa', artist_name: 'Toto', rating: 92}
+        {id: 4, title: 'Fur Elise', artist_name: 'Ludwig Van Beethoven'}
       ]);
     });
 };
