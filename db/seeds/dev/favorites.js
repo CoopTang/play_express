e5c84075feb0931dc04b12dd0a07d5b5a51c8303
Jon Patterson <JonPatt92@gmail.com>
2020-02-04 10:56:19 -0700

    Create initial seeds file for favorites, one record missing genre to later test the default value for that column

---
+++ db/seeds/dev/favorites.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('favorites').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('favorites').insert([
+        {id: 1, title: 'Take On Me', artist_name: 'a-ha', rating: 87, genre: 'Pop'},
+        {id: 2, title: 'Coming Home', artist_name: 'Leon Bridges', rating: 90, genre: 'Soul'},
+        {id: 3, title: 'Africa', artist_name: 'Toto', rating: 92}
+      ]);
+    });
+};
