7a03ff407ca2178917e3e5a36932440cdb0fd303
Michael Cooper <macoop2363@gmail.com>
2020-02-05 14:49:05 -0700

    Update tests due to the MusixMatch API no longer having the test song

--- tests/services/musix_match_service.spec.js
+++ tests/services/musix_match_service.spec.js
@@ -4,14 +4,22 @@ var musixMatchService = require('../../lib/services/musix_match_service');
 
 describe('Test ', () => {
   test('It should get a json response from the MusixMatch API', async () => {
-    let response = await musixMatchService.getTracks("We Will Rock You", "Queen")
+    let response = await musixMatchService.getTracks("Sweet Emotion", "Aerosmith")
 
     expect(response).toHaveProperty('message')
     expect(response.message).toHaveProperty('body')
     expect(response.message.body).toHaveProperty('track')
     expect(response.message.body.track).toHaveProperty('track_name')
-    expect(response.message.body.track.track_name).toBe('We Will Rock You')
+    expect(response.message.body.track.track_name).toBe('Sweet Emotion')
     expect(response.message.body.track).toHaveProperty('artist_name')
-    expect(response.message.body.track.artist_name).toBe('Queen')
+    expect(response.message.body.track.artist_name).toBe('Aerosmith')
+  });
+
+  test('It should get an empty body json response from the MusixMatch API', async () => {
+    let response = await musixMatchService.getTracks("", "")
+
+    expect(response).toHaveProperty('message')
+    expect(response.message).toHaveProperty('body')
+    expect(response.message.body).toBe("")
   });
 });
